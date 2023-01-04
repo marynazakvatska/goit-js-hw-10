3571f6ea568ac8af5c4c78801318482019a4ee75
marynazakvatska <maryna.zakvatska@gmail.com>
2023-01-04 17:17:16 +0100

    template

--- src/index.js
+++ src/index.js
@@ -1,74 +1,46 @@
-
 import './css/styles.css';
 
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchCountries } from './fetchCountries';
-
+import descriptionTemplate from './description-card.hbs';
+import listItemTemplate from './listTemplate.hbs';
 
 const DEBOUNCE_DELAY = 300;
 
-const inputEl = document.querySelector("input#search-box")
-const listEl = document.querySelector(".country-list")
-const descrEl = document.querySelector(".country-info")
+const inputEl = document.querySelector('input#search-box');
+const listEl = document.querySelector('.country-list');
+const descrEl = document.querySelector('.country-info');
 
-
-inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY))
-/* inputEl.addEventListener('input', onInput) */
-
-
+inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(e) {
-    const formRef = e.currentTarget;
-    console.log(formRef.value); 
-    if (formRef.value === "" ) {
-        listEl.innerHTML = "";
-         descrEl.innerHTML = "";
-     return
-    }
+  if (e.target.value === '') {
+    listEl.innerHTML = '';
+    descrEl.innerHTML = '';
+    return;
+  }
 
-fetchCountries(formRef.value)
+  fetchCountries(e.target.value)
     .then(countries => {
-        console.log(countries)
- 
-        for (const country of countries) {
-            const { flag, name, capital, population, languages } = country;
-            if (countries.length >= 10) {
-        listEl.innerHTML = "";
-         descrEl.innerHTML = "";
-return  Notify.info("Too many matches found. Please enter a more specific name.");
-
-            }
-            else if (countries.length < 10 && countries.length >= 2) {
-                descrEl.innerHTML =''
-                listEl.insertAdjacentHTML("beforeend", `
-<ul>
-<li><image src = "${flag}"  alt = "flag of ${name}"  width = 80px></image> </li>
-<li>${name}</li>
-</ul>`
-   
-)
-       }
-            else if (countries.length === 1) {
-    listEl.innerHTML = ""
-      descrEl.insertAdjacentHTML("beforeend",  `<ul>
-
-<li><image src = "${flag}"  alt = "flag of ${name}"  width = 80></image> </li>
-<li>${name}</li>
-<li>Capital : ${capital} </li>
-<li>Population: ${population}</li>
-<li>Languages: ${languages}</li>
-
-</ul>`)    
-
-            }
-        }
+      if (countries.length >= 10) {
+        listEl.innerHTML = '';
+        descrEl.innerHTML = '';
+        return Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (countries.length < 10 && countries.length >= 2) {
+        descrEl.innerHTML = '';
+        listEl.innerHTML = listItemTemplate({ countries });
+      } else if (countries.length === 1) {
+        listEl.innerHTML = '';
+        descrEl.innerHTML = descriptionTemplate(countries[0]);
+      }
     })
-    .catch(onFetchError)
-
-  
-};
+    .catch(onFetchError);
+}
+console.log('hello');
 
 function onFetchError(error) {
-return Notify.failure("Oops, there is no country with that name");
+  return Notify.failure('Oops, there is no country with that name');
 }
